16382d87a7e0f118f34f8986f502fa1c7be3c83f
Brithiv <bkbrithivin@gmail.com>
2023-11-28 21:42:44 +0530

    Biruthivin coding

---
+++ src/org/todayGit/TodayGit.java
@@ -0,0 +1,14 @@
+package org.todayGit;
+
+public class TodayGit {
+public static void main(String[] args) {
+	
+	
+	System.out.println("Biruthivin is back to action");
+	
+	
+	System.out.println("A is on duty");
+	System.out.println("B is on duty");
+	System.out.println("C is on duty");
+}
+}
